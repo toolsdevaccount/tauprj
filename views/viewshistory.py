a291f5d95237b8a5dfce139a4c51334f3b94bd94
toolsdevaccount <sakurai79@hotmail.co.jp>
2024-09-05 13:43:14 +0900

    20240905 行方不明views対応

--- views/viewshistory.py
+++ views/viewshistory.py
@@ -1,172 +1,117 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, TemplateView
-from myapp.models import OrderingTable, OrderingDetail, RequestResult
+from django.shortcuts import render
+from django.views.generic import ListView 
+from myapp.models import CustomerSupplier,OrderingDetail, RequestResult
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Case, When, Value, CharField
-from myapp.form.formsrequestresult import RequestResultForm, HistorySearchForm
-# 検索機能のために追加
+from myapp.form.formshistory import historyManageForm
+
+# models
 from django.db.models import Q
+# ajax
+from django.http import JsonResponse
+
 #LOG出力設定
 import logging
 logger = logging.getLogger(__name__)
 
 # 受発注一覧/検索
-class HistoryListView(LoginRequiredMixin,ListView):
-    model = OrderingTable
-    form_class = RequestResultForm
-    context_object_name = 'object_list'
-    queryset = OrderingTable.objects.select_related()
+class HistoryManageView(LoginRequiredMixin,ListView):
+    model = CustomerSupplier
+    form_class =  historyManageForm
     template_name = "crud/history/list/historylist.html"
-    paginate_by = 20
 
-    def post(self, request, *args, **kwargs):
-        search = [
-            self.request.POST.get('query', None),
-            self.request.POST.get('key', None),
-            self.request.POST.get('word', None),
-            self.request.POST.get('historydateFrom', None),
-            self.request.POST.get('historydateTo', None),
-        ]
-        if 'clear' in request.POST:
-            del request.session['historysearch']
-        else:
-            request.session['historysearch'] = search
-        # 検索時にページネーションに関連したエラーを防ぐ
-        self.request.GET = self.request.GET.copy()
-        self.request.GET.clear()
+    def get_context_data(self, **kwargs):
+        context = super(HistoryManageView, self).get_context_data(**kwargs)
 
-        return self.get(request, *args, **kwargs)
+        return context
 
-    #検索機能
-    def get_queryset(self):
-        if 'historysearch' in self.request.session:
-            search = self.request.session['historysearch']
-            query = search[0]
-            key = search[1]
-            word = search[2]
-            historydateFrom = search[3]
-            historydateTo = search[4]
-        else:
-            query = self.request.POST.get('query', None)
-            key = self.request.POST.get('key', None)
-            word = self.request.POST.get('word', None)
-            historydateFrom = self.request.POST.get('historydateFrom', None)
-            historydateTo = self.request.POST.get('historydateTo', None)
-
-        # 依頼日、伝票区分、オーダーNO大きい順で抽出
-        queryset = OrderingTable.objects.order_by('OrderingDate','SlipDiv','OrderNumber').reverse()
-        # 削除済以外、管理者の場合は全レコード表示（削除済以外）
-        if self.request.user.is_superuser == 0:
-            #queryset = queryset.filter(is_Deleted=0,Created_id=self.request.user.id)
-            # 全ユーザ表示
-            queryset = queryset.filter(is_Deleted=0)
-        else:
-            queryset = queryset.filter(is_Deleted=0)
-
-        if query:
-            queryset = queryset.filter(
-                Q(SampleDiv__divname__icontains=query) | Q(OutputDiv__outputdivname__icontains=query) | Q(ManagerCode__first_name__icontains=query) |
-                Q(ProductName__icontains=query) | Q(DestinationCode__CustomerOmitName__icontains=query) | Q(SupplierCode__CustomerOmitName__icontains=query) |
-                Q(ShippingCode__CustomerOmitName__icontains=query) | Q(CustomeCode__CustomerOmitName__icontains=query) | Q(RequestCode__CustomerOmitName__icontains=query) |
-                Q(SlipDiv__contains=query) | Q(OrderNumber__icontains=query)
-            )
-        if key:
-            queryset = queryset.filter(
-                Q(SampleDiv__divname__icontains=key) | Q(OutputDiv__outputdivname__icontains=key) | Q(ManagerCode__first_name__icontains=key) |
-                Q(ProductName__icontains=key) | Q(DestinationCode__CustomerOmitName__icontains=key) | Q(SupplierCode__CustomerOmitName__icontains=key) |
-                Q(ShippingCode__CustomerOmitName__icontains=key) | Q(CustomeCode__CustomerOmitName__icontains=key) | Q(RequestCode__CustomerOmitName__icontains=key) |
-                Q(SlipDiv__contains=key) | Q(OrderNumber__icontains=key) 
-            )
-
-        if word:
-            queryset = queryset.filter(
-                Q(SampleDiv__divname__icontains=word) | Q(OutputDiv__outputdivname__icontains=word) | Q(ManagerCode__first_name__icontains=word) |
-                Q(ProductName__icontains=word) | Q(DestinationCode__CustomerOmitName__icontains=word) | Q(SupplierCode__CustomerOmitName__icontains=word) |
-                Q(ShippingCode__CustomerOmitName__icontains=word) | Q(CustomeCode__CustomerOmitName__icontains=word) | Q(RequestCode__CustomerOmitName__icontains=word) | 
-                Q(SlipDiv__contains=word) | Q(OrderNumber__icontains=word) 
-            )
-
-        if historydateFrom and historydateTo:
-            queryset = queryset.filter(Q(OrderingDate__range=(historydateFrom,historydateTo)))
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset(**kwargs)
 
         return queryset
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # sessionに値がある場合、その値をセットする。（ページングしてもform値が変わらないように）
-        query = ''
-        key = ''
-        word = ''
-        historydateFrom = ''
-        historydateTo = ''
-        if 'historysearch' in self.request.session:
-            search = self.request.session['historysearch']
-            query = search[0]
-            key = search[1]
-            word = search[2]
-            historydateFrom = search[3]
-            historydateTo = search[4]
+    def List(request, OrderNumber1, OrderNumber2, OrderNumber3):
+        if OrderNumber1!='0':
+            Condition_OrderNumber1 = Q(OrderingTableId__OrderNumber=OrderNumber1)
+        else:
+            Condition_OrderNumber1 = Q()
+        #出荷先
+        if OrderNumber2!='0':
+            Condition_OrderNumber2 = Q(OrderingTableId__OrderNumber=OrderNumber2)
+        else:
+            Condition_OrderNumber2 = Q()
+        #依頼先
+        if OrderNumber3!='0':
+            Condition_OrderNumber3 = Q(OrderingTableId__OrderNumber=OrderNumber3)
+        else:
+            Condition_OrderNumber3 = Q()
 
-        default_data = {'query': query,
-                        'key': key,
-                        'word': word,
-                        'historydateFrom': historydateFrom,
-                        'historydateTo': historydateTo,
-                       }
-        
-        form = HistorySearchForm(initial=default_data) # 検索フォーム
-        context['historysearch'] = form
-        return context
-
-# 受発注情報編集
-class HistoryDetailView(LoginRequiredMixin,TemplateView):
-    model = RequestResult
-    template_name = "crud/history/update/historyform.html"
-
-    # get_context_dataをオーバーライド
-    def get_context_data(self, **kwargs):
-        pk = self.kwargs.get("pk")
-        queryset = RequestResult.objects.filter(is_Deleted=0, OrderingId_id=pk)       
-        key = queryset.values_list("OrderingDetailId_id",flat=True).first()
-
-        querysetdetail = OrderingDetail.objects.values(
+        queryset =  OrderingDetail.objects.values(
             'id',
+            'OrderingTableId__id',
+            'OrderingTableId__SlipDiv',
+            'OrderingTableId__OrderNumber',
             'DetailItemNumber',
-            'DetailColorNumber',
-            'DetailColor',
-            'DetailTailoring',
-            'DetailVolume',
-            'DetailUnitPrice',
-            'DetailPrice',
-            'DetailOverPrice',
-            'DetailSellPrice',
-        ).filter(
-            is_Deleted=0,
-            OrderingTableId=pk
-        ).annotate(
-             DetailUnitDiv=Case( 
-                When(DetailUnitDiv=0, then=Value('')),
-                When(DetailUnitDiv=1, then=Value('Kg')),
-                When(DetailUnitDiv=2, then=Value('本')),
-             default=Value(''), 
-             output_field=CharField()) 
-        )
-
-        querysetresult = RequestResult.objects.values(
-            'id',
-            'ResultDate',
-            'ResultItemNumber',
-            'ShippingDate',
-            'ShippingVolume',
-            'SlipNumber',
-        ).filter(
-            is_Deleted=0,
-            OrderingId=pk
-        )
+            'OrderingTableId__ProductName',
+            'OrderingTableId__OrderingCount',
+            'OrderingTableId__DestinationCode_id__CustomerCode',
+            'OrderingTableId__DestinationCode_id__CustomerName',
+            'OrderingTableId__ShippingCode_id__CustomerCode',
+            'OrderingTableId__ShippingCode_id__CustomerName',
+            'OrderingTableId__RequestCode_id__CustomerCode',
+            'OrderingTableId__RequestCode_id__CustomerName',
+            ).filter(Q(Condition_OrderNumber1) | Q(Condition_OrderNumber2) |Q(Condition_OrderNumber3) , is_Deleted=0
+                    ).order_by(
+                        'OrderingTableId__SlipDiv',
+                        'OrderingTableId__OrderNumber',
+                    )
 
         context = {
-            'formset': querysetdetail,
-            'inlinesRecord': querysetresult,
+            'form': queryset,
         }
 
-        return context
+        return render(request, 'crud/history/list/historylist.html', context)
+    
+    def history_result_ajax(request):
+        if request.method == 'GET':  # GETの処理
+            table_param = request.GET.get("tableid")  # GETパラメータ
+            detail_param = request.GET.get("detailid")  # GETパラメータ
+            #
+            detail = list(RequestResult.objects.values(
+                'id',
+                'OrderingId__id',
+                'OrderingDetailId__id',
+                'OrderingId__OrderingDate',
+                'OrderingDetailId__DetailItemNumber',
+                'OrderingDetailId__DetailColorNumber',
+                'OrderingDetailId__DetailColor',
+                'OrderingDetailId__DetailTailoring',
+                'OrderingDetailId__DetailVolume',
+                'OrderingDetailId__DetailUnitPrice',
+                'OrderingDetailId__DetailPrice',
+                'OrderingDetailId__DetailOverPrice',
+                'OrderingDetailId__DetailSellPrice',
+                'ResultDate',
+                'ResultItemNumber',
+                'ShippingDate',
+                'ShippingVolume',
+                'SlipNumber',
+                'OrderingId__DestinationCode_id__CustomerCode',
+                'OrderingId__DestinationCode_id__CustomerName',
+                'OrderingId__SupplierCode_id__CustomerCode',
+                'OrderingId__SupplierCode_id__CustomerName',
+                'OrderingId__ShippingCode_id__CustomerCode',
+                'OrderingId__ShippingCode_id__CustomerName',
+                'OrderingId__CustomeCode_id__CustomerCode',
+                'OrderingId__CustomeCode_id__CustomerName',
+                'OrderingId__RequestCode_id__CustomerCode',
+                'OrderingId__RequestCode_id__CustomerName',
+                'OrderingId__RequestCode_id__ManagerCode',
+                'OrderingId__RequestCode_id__ManagerCode__first_name',
+                'OrderingId__RequestCode_id__ManagerCode__last_name',
+                ).filter(OrderingId_id=table_param,OrderingDetailId_id=detail_param,is_Deleted=0))
+        
+            context = {
+                'list': detail,
+            }
+
+            return JsonResponse(context)
